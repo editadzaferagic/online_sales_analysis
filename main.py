2dc7763db25a34471b4f5815137a437f847013d6
editadzaferagic <edita.dzaferagic@outlook.com>
2025-06-12 21:10:36 +0200

    Faza 6: Spojene promjene sa klasom Cart i rješeni konflikti

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from product import Product
 from product_manager import ProductManager
 
 pm = ProductManager()
-pm.add_product(Product("Laptop", 1200, 3))
+pm.add_product(Product("Gaming Laptop", 1200, 3))
 pm.add_product(Product("Mouse", 25, 10))
 pm.add_product(Product("Keyboard", 60, 4))
-
-pm.show_all_products()
-print("Total Inventory Value:", pm.total_inventory_value())
